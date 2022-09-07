f0d0ef24d6dbf019505cd515aa89342c140275f8
smslsmls <skipp1223@naver.com>
2022-09-07 15:50:20 +0900

    Update 소스.cpp

--- Report2/소스.cpp
+++ Report2/소스.cpp
@@ -1,29 +1,31 @@
 #include<bits/stdc++.h>
+using ll = long long;
 using namespace std;
+
+double interest_array[2] = { 3,4 };
 
 class Saving {
 private:
 	double interest;
-	int deposit;
+	ll deposit;
 	double tax = 0.154;
-	double interest_array[2] = { 3.0,4.0 };
 public:
-	Saving(double interest, int deposit) :interest(interest), deposit(deposit) {}
+	Saving(double interest, ll deposit) :interest(interest), deposit(deposit) {}
 
-	Saving(int saving_type, int deposit) :Saving(interest_array[saving_type], deposit) {}
+	Saving(int saving_type, ll deposit) :Saving(interest_array[saving_type], deposit) {}
 
 	void calc(int year) {
 		double inter = ((interest / 100) * ((year * 12 + 1) / 24.0)) * (deposit * 12);
-		cout << (int)inter + (deposit * 12 * year) << '\n';
+		cout << (ll)inter + (deposit * 12 * year) << '\n';
 		inter *= 1 - tax;
-		cout << (int)inter + (deposit * 12 * year) << '\n';
+		cout << (ll)inter + (deposit * 12 * year) << '\n';
 	}
 
-	//void get_info() const {
-	//	cout << "interest : " << interest << '\n';
-	//	cout << "deposit for month : " << deposit << '\n';
-	//	cout << "tax : " << tax << '\n';
-	//}
+	void get_info() const {
+		cout << "interest : " << interest << '\n';
+		cout << "deposit for month : " << deposit << '\n';
+		cout << "tax : " << tax << '\n';
+	}
 };
 
 int main() {
@@ -31,6 +33,6 @@ int main() {
 	cout << "일반 : 0\n특판 : 1\n";
 	cin >> saving_type >> depos >> year;
 	Saving saving(saving_type, depos);
-	//saving.get_info();
+	saving.get_info();
 	saving.calc(year);
 }
