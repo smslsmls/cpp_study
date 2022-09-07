72153f122dc517f6a3aa0fbec2a173b29e71fb9a
smslsmls <skipp1223@naver.com>
2022-09-07 15:56:45 +0900

    Update 소스.cpp

--- Report2/소스.cpp
+++ Report2/소스.cpp
@@ -14,25 +14,25 @@ public:
 
 	Saving(int saving_type, ll deposit) :Saving(interest_array[saving_type], deposit) {}
 
-	void calc(int year) {
-		double inter = ((interest / 100) * ((year * 12 + 1) / 24.0)) * (deposit * 12);
-		cout << (ll)inter + (deposit * 12 * year) << '\n';
+	void calc(int month) {
+		double inter = ((interest / 100) * ((month + 1) / 24.0)) * (deposit * 12);
+		cout << (ll)inter + (deposit * month) << '\n';
 		inter *= 1 - tax;
-		cout << (ll)inter + (deposit * 12 * year) << '\n';
+		cout << (ll)inter + (deposit * month) << '\n';
 	}
 
-	void get_info() const {
-		cout << "interest : " << interest << '\n';
-		cout << "deposit for month : " << deposit << '\n';
-		cout << "tax : " << tax << '\n';
-	}
+	//void get_info() const {
+	//	cout << "interest : " << interest << '\n';
+	//	cout << "deposit for month : " << deposit << '\n';
+	//	cout << "tax : " << tax << '\n';
+	//}
 };
 
 int main() {
-	int saving_type, depos, year;
+	int saving_type, depos, month;
 	cout << "일반 : 0\n특판 : 1\n";
-	cin >> saving_type >> depos >> year;
+	cin >> saving_type >> depos >> month;
 	Saving saving(saving_type, depos);
-	saving.get_info();
-	saving.calc(year);
+	//saving.get_info();
+	saving.calc(month);
 }
